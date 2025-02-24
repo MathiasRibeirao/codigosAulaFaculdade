0086d87fbd13c28a561951fc7e3cb3a815587f42
MathiasRibeirao <85964708+MathiasRibeirao@users.noreply.github.com>
2025-02-24 16:21:16 -0300

    exerciciosAulaMaromo

--- main.c
+++ main.c
@@ -1,350 +1,31 @@
-/*
- *ex2
- *#include <stdio.h>
+/*#include <stdio.h>
 
 int main() {
-
-    int num;
-
-    printf("digite um numero: \n");
-    scanf("%d", &num);
-
-    printf("O numero digitado foi: %d\n", num);
-    return 0;
-}
-*/
-//ex3
-/*
-#include <stdio.h>
-
-int main() {
-
-    float num1, num2, soma;
-
-    printf("digite um numero: \n");
-    scanf("%f", &num1);
-
-    printf("digite outro numero: \n");
-    scanf("%f", &num2);
-
-    soma = num1 + num2;
-
-    printf("a soma dos numeros: %0.3f\n", soma);
-    return 0;
-}
-*/
-//ex4
-/*
-#include <stdio.h>
-
-int main() {
-    float n1, n2, n3, n4, media;
-
-    printf("digite um numero: \n");
-    scanf("%f", &n1);
-
-    printf("digite outro numero: \n");
-    scanf("%f", &n2);
-
-    printf("digite outro numero: \n");
-    scanf("%f", &n3);
-
-    printf("digite outro numero: \n");
-    scanf("%f", &n4);
-
-
-
-    media = n1 + n2 +n3 + n4;
-    media = media / 4;
-
-    printf("a media bimestral: %0.3f\n", media);
-    return 0;
-}
-*/
-
-//ex5
-/*
-#include <stdio.h>
-
-int main() {
-
-    float metros;
-
-    printf("digite os metros: \n");
-    scanf("%f", &metros);
-
-    metros = metros * 100;
-
-    printf("de metro para centimetros da eh: %0.3f\n", metros);
-    return 0;
-}
-*/
-//ex6
-/*
-#include <stdio.h>
-
-int main() {
-
-    float raio, area;
-
-    printf("digite um raio: \n");
-    scanf("%f", &raio);
-
-    area = 3.14 * raio * raio;
-
-    printf("O valor de area eh: %0.3f\n", area);
+    printf("Tamanhos dos tipos de dados em bytes:\n");
+    printf("char: %zu bytes\n", sizeof(char));
+    printf("int: %zu bytes\n", sizeof(int));
+    printf("float: %zu bytes\n", sizeof(float));
+    printf("double: %zu bytes\n", sizeof(double));
+    printf("long double: %zu bytes\n", sizeof(long double));
 
     return 0;
 }
 */
-//ex7
 /*
 #include <stdio.h>
+#include <math.h>
 
 int main() {
+    double raio, area, perimetro;
 
-    float lado, area;
+    printf("Insira o valor do raio de um circulo: \n");
+    scanf("%lf", &raio);
 
-    printf("digite o valor do lado: \n");
-    scanf("%f", &lado);
+    area = M_PI * (raio * raio);
+    perimetro = 2 * M_PI * raio;
 
-    area = lado * lado;
-
-    printf("O valor de area eh: %0.3f\n", area);
-
-    return 0;
-}
-*/
-//ex8
-/*
-#include <stdio.h>
-
-int main() {
-
-    float horas, valor, salario;
-
-    printf("digite o valor pago por hora: \n");
-    scanf("%f", &valor);
-
-    printf("digite as horas trabalhadas no mes: \n");
-    scanf("%f", &horas);
-
-    salario = valor * horas;
-
-    printf("o seu salario total e: %0.3f\n",salario);
-
-    return 0;
-}
-*/
-//ex9
-/*
-#include <stdio.h>
-
-int main() {
-
-    float fah, celsius;
-
-    printf("digite a temperatura em Fahrenheit: \n");
-    scanf("%f", &fah);
-
-
-
-    celsius = (fah-32)*5/9;
-
-    printf("a temperatura convertida para celsius: %0.3f\n",celsius);
-
-    return 0;
-}
-*/
-//ex10
-/*
-#include <stdio.h>
-
-int main() {
-
-    float fah, celsius;
-
-    printf("digite a temperatura em celsius: \n");
-    scanf("%f", &celsius);
-
-
-
-    fah = celsius * 9 / 5 + 32;
-
-    printf("a temperatura convertida para Fahrenheit: %0.3f\n",fah);
-
-    return 0;
-}
-*/
-//ex11
-/*
-#include <stdio.h>
-
-int main() {
-    float r3, n3;
-    int n1, n2, r1, r2;
-
-    printf("digite o primeiro valor inteiro: \n");
-    scanf("%d", &n1);
-
-    printf("digite o segundo valor inteiro: \n");
-    scanf("%d", &n2);
-
-    printf("digite o terceiro valor real: \n");
-    scanf("%f", &n3);
-
-    r1 = (n1 * 2) + (n2 / 2);
-    r2 = (n1 * 3) + n3;
-    r3 = n3 * n3 * n3;
-
-
-    printf("resultado da primera parada: %d\n", r1);
-    printf("resultado da segunda parada: %d\n", r2);
-    printf("resultado da terceira parada: %0.3f\n", r3);
-    return 0;
-}
-*/
-//ex12
-/*
-#include <stdio.h>
-
-int main() {
-    float imc, altura;
-
-    char genero;
-
-    //printf("digite o seu peso: \n");
-    //scanf("%d", &peso);
-
-    printf("digite sua altura: \n");
-    scanf("%f", &altura);
-
-    printf("digite seu genero: \n");
-    scanf("%s", &genero);
-
-    if (genero == 'H') {
-        imc = 72.7 * (altura / 100) - 58;
-        printf("O seu peso ideal: %.2f\n", imc);
-    } else if (genero == 'M') {
-        imc = (62.1 * altura) - 58;
-        printf("O seu peso ideal: %.2f\n", imc);
-    } else
-        printf("algo deu errado");
-
-
-    return 0;
-}
-*/
-//Ex13
-/*
-#include <stdio.h>
-
-int main() {
-    float peso, altura;
-
-
-    printf("digite sua altura: \n");
-    scanf("%f", &altura);
-
-
-    peso = 72.7 * (altura/100) - 58;
-
-    printf("seu peso ideal:  %.2f\n",peso);
-
-
-    return 0;
-}
-*/
-//ex14
-/*
-#include <stdio.h>
-
-
-int main() {
-    float peso, multa, excesso;
-
-
-    printf("digite os kilos pescados: \n");
-    scanf("%f", &peso);
-
-
-    if (peso > 50) {
-
-        excesso = peso - 50 ;
-        multa = excesso * 4;
-    }
-
-    printf("o valor da multa: R$ %0.2f\n",multa);
-
-
-    return 0;
-}
-*/
-//ex15
-/*
-#include <stdio.h>
-
-int main() {
-    float salario, ir, inss, sindicato, salario_liquido;
-    float valorHora, horas;
-
-    printf("digite o valor pago por hora: \n");
-    scanf("%f", &valorHora);
-
-    printf("digite as horas trabalhadas: \n");
-    scanf("%f", &horas);
-
-    salario = valorHora * horas;
-
-    printf("digite a porcentagem paga ao Imposto de renda: \n");
-    scanf("%f", &ir);
-
-    printf("digite a porcentagem paga ao INSS: \n");
-    scanf("%f", &inss);
-
-    printf("digite a porcentagem paga ao Sindicato: \n");
-    scanf("%f", &sindicato);
-
-
-    ir = salario * (ir / 100.0);
-    inss = salario * (inss / 100.0)  ;
-    sindicato =  salario * (sindicato / 100.0);
-
-
-    salario_liquido = salario - (ir + inss + sindicato);
-
-
-    printf("o seu salario com os descontos: R$ %0.2f\n", salario_liquido);
-    printf("valor de imposto de renda: R$ %0.2f\n", ir);
-    printf("valor de inss: R$ %0.2f\n", inss);
-    printf("valor de sindicato: R$ %0.2f\n", sindicato);
-    printf("o seu salario bruto: R$ %0.2f\n", salario);
-
-
-    return 0;
-}
-*/
-//ex16
-/*
-#include <stdio.h>
-
-int main() {
-    float metros,valorTotal, litros, qtdLatas;
-
-
-    printf("digite em metros quadrados o tamanho da area: \n");
-    scanf("%f", &metros);
-
-
-    litros = metros /  3;
-    qtdLatas = litros / 18;
-    valorTotal = qtdLatas * 80;
-
-    printf("preço total: R$ %0.2f\n", valorTotal);
-    printf("quantidade de latas %0.2f\n", qtdLatas);
-
-
-
+    printf("Este e o valor da area do seu raio: %0.2lf  \n", area);
+    printf("Este e o valor do perimetro do seu raio: %0.2lf  \n", perimetro);
 
     return 0;
 }
@@ -352,48 +33,127 @@ int main() {
 /*
 #include <stdio.h>
 
+
 int main() {
+    char primeiroCaracter, segundoCaracter;
 
-    int i, n1;
-    printf("Digite um numero: \n");
-    scanf("%d", &n1);
+    printf("Digite seu primeiro caracter:  ");
+    scanf("%c", &primeiroCaracter);
+    getchar();
+    printf("\nDigite seu segundo caracter: ");
+    scanf("%c", &segundoCaracter);
 
-    if (n1 % n1) {}
+
+    printf("\nseu primeiro caracter: \'%c\' \n", primeiroCaracter);
+    printf("\nseu segundo caracter: \'%c\' \n", segundoCaracter);
 
     return 0;
 }
 */
-//ex
 /*
 #include <stdio.h>
 
+
 int main() {
-    int i, n1 ;
-    int ehPrimo = 1; // Inicializa como 1 (primo)
+    int valorDigitado, valorTransformadoFloat;
+    float valorFinal;
 
-    printf("Digite um numero: \n");
-    scanf("%d", &n1);
+    printf("\nDigite o valor inteiro: ");
+    scanf("%d",&valorDigitado);
 
-    // Verifica se o número é menor que 2
-    if (n1 < 2) {
-        ehPrimo = 0; // Não é primo
+    valorTransformadoFloat = (float) valorDigitado;
+    valorFinal = valorTransformadoFloat * 0.5;
+    valorFinal = (int) valorFinal;
+
+    printf("\nO valor final e: %f", valorFinal);
+
+    return 0;
+}
+*/
+/*
+#include <stdio.h>
+
+
+int main() {
+    int valorDigitado;
+
+    printf("insira um valor: \n");
+    scanf("%d", &valorDigitado);
+
+    if (valorDigitado % 2 == 0) {
+        printf("numero e par.\n");
     } else {
-        // Verifica se o número é divisível por algum número entre 2 e n1 - 1
-        for (i = 2; i <= n1 / 2; i++) {
-            if (n1 % i == 0) { // Se encontrar um divisor
-                ehPrimo = 0; // Não é primo
-                break; // Sai do laço
-            }
-        }
-    }
-
-    if (ehPrimo) {
-        printf("%d u primo.\n", n1);
-    } else {
-        printf("%d não e primo.\n", n1);
+        printf("numero impar. \n");
     }
 
     return 0;
 }
-
 */
+/*
+#include <stdio.h>
+
+
+int main() {
+    float nota1, nota2, nota3, media;
+
+    printf("Insira sua primeira nota: ");
+    scanf("%f", &nota1);
+
+    printf("Insira sua segunda nota: ");
+    scanf("%f", &nota2);
+
+    printf("Insira sua terceira nota: ");
+    scanf("%f", &nota3);
+
+
+    media = (nota1 + nota2 + nota3) / 3;
+
+
+    if (media >= 7) {
+        printf("Aprovado");
+    } else {
+        printf("Sua nota foi tao bosta que nem vou te falar nada rsrsrsrsr");
+    }
+    return 0;
+}
+*/
+/*
+#include <stdio.h>
+
+
+int main() {
+    float nota1, nota2, nota3, media;
+
+    printf("Insira sua primeira nota: ");
+    scanf("%f", &nota1);
+
+    printf("Insira sua segunda nota: ");
+    scanf("%f", &nota2);
+
+    printf("Insira sua terceira nota: ");
+    scanf("%f", &nota3);
+
+
+    media = (nota1 + nota2 + nota3) / 3;
+
+    media >= 7 ? printf("Aprovado") : printf("Reprovado");
+
+    return 0;
+}
+*/
+#include <stdio.h>
+int main(void) {
+    float numeroFatorial, contador, resultado = 1;
+
+    printf("Digite o numero que deseja fazer o fatorial:  ");
+    scanf("%f",&contador);
+
+
+    while (contador > 0) {
+        resultado = resultado * contador;
+        contador--;
+    }
+    printf("seu valor fatorial:  %0.0f\n",resultado);
+
+    return 0;
+}
